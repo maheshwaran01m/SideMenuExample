7330022a991b201cae8675d2a00bb3b262165938
MAHESHWARAN <maheshwaran@eam360.com>
2024-03-30 12:37:36 +0530

    Source  - First Commit

--- SideMenuExample/MainResource/SideMenuExampleApp.swift
+++ SideMenuExample/MainResource/SideMenuExampleApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SideMenuExampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }

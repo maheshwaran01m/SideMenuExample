3bd745039c5d71476c2a09344bc6bfd3a44df68a
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-03-30 12:36:00 +0530

    Initial Commit

---
+++ SideMenuExample/SideMenuExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuExampleApp.swift
+//  SideMenuExample
+//
+//  Created by MAHESHWARAN on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
